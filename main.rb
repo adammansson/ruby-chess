8d0a109ffec6d36d2c355a509713773afe810db8
Adam Månsson <adam.s.mansson@gmail.com>
2022-07-12 14:40:43 +0200

    Add main file for running the program

---
+++ main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative 'lib/board'
+
+# board = Board.from_fen('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+board = Board.from_fen('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2')
+puts board
