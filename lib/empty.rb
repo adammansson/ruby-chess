000bd36200c479368402f8d546f6974265467176
Adam Månsson <adam.s.mansson@gmail.com>
2022-07-19 21:58:54 +0200

    Add color attribute to Empty and OffBoard classes

--- lib/empty.rb
+++ lib/empty.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Empty
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+
   def to_s
     ' '
   end
--- lib/off_board.rb
+++ lib/off_board.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class OffBoard
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
 end
