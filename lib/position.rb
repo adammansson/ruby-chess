02a4925d67c43160cce27fac727c91a8c451a7ce
Adam Månsson <adam.s.mansson@gmail.com>
2022-07-19 22:00:57 +0200

    Fledge out Position class, from_string valid? == etc.

--- lib/position.rb
+++ lib/position.rb
@@ -8,6 +8,42 @@ class Position
     @file = file
   end
 
+  def self.from_string(string)
+    array = string.split('')
+    rank = array[1].to_i - 1
+    file = array[0].ord - 'a'.ord
+    Position.new(rank, file)
+  end
+
+  def self.valid?(position)
+    array = position.split('')
+    if array.length != 2
+      puts "Input wasn't a rank and a file"
+      false
+    elsif !array[0].between?('a', 'h')
+      puts 'Rank out of range'
+      false
+    elsif !array[1].between?('1', '8')
+      puts 'File out of range'
+      false
+    else
+      true
+    end
+  end
+
+  def ==(position)
+    @rank == position.rank && @file == position.file
+  end
+
+  def add(rank, file)
+    Position.new(@rank + rank, @file + file)
+  end
+
+  def update(position)
+    @rank = position.rank
+    @file = position.file
+  end
+
   def to_s
     "#{('a'.ord + @file).chr}#{@rank + 1}"
   end
